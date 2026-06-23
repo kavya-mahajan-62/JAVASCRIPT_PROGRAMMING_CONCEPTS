80ab8c11b878e6e80f7166c9e41500d7195e973a
Kavya Mahajan <mahajankavya76@gmail.com>
2026-06-23 12:45:49 +0530

    ++

--- app.js
+++ app.js
@@ -13,5 +13,8 @@ let eraserPrice = 20;
 console.log("the total price is :", pencilPrice + eraserPrice, "Rupees.");
 
 //let us create output string
-let output = "the total price is :" + (pencilPrice + eraserPrice) + "Rupees.";
+let output = "the total price is :" + (pencilPrice + eraserPrice) + "Rupees."; //+ operator is used to concatenate the string and the numbers but we have to use parenthesis to group the expression otherwise it will give us the wrong output because of operator precedence and concatenation means joining the string and the numbers together to form a single string.
 console.log(output); //we cannot use comma in output string because it will not give us the correct output  parenthesis are used to group the expression and give us the correct output otherwise it will give us the wrong output because of operator precedence
+//let us create output string using template literals
+console.log(`the total price is : ${pencilPrice + eraserPrice} Rupees. `);//difference between comma and + operator is that comma operator will print the values with a space in between them and + operator will concatenate the values without any space in between them. Template literals are used to create output string in a more readable and easier way. We can use backticks `` to create template literals and we can use ${} to insert the values of variables or expressions inside the template literals.
+
